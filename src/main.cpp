6d8c0cbfd2b3dd733707842a30aa31f308fbfa7d
GluTbl <glusagolsem@gmail.com>
2020-03-21 01:00:41 +0530

    Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+extern "C"
+{
+#include "user_interface.h"
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+uint16_t TCP_PORT = 8080;
+
+void presetup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("\nFree Ram at presetup:");
+  uint32_t free = system_get_free_heap_size();
+  Serial.print(free);
+}
+
+void postsetup() {}
+
+/////////////////////
+char host[] = "192.168.29.242";
+int port = 12345;
+
+unsigned long mili = 0;
+void loop()
+{
+  if (millis() - mili > 2000)
+  {
+    mili = millis();
+    uint32_t free = system_get_free_heap_size();
+    Serial.print("\nFree Ram:");
+    Serial.print(free);
+    connectToServer(host, port);
+  }
 }
